f90ee4ef87891264e2e1dc94b4d433bb3d0bee02
Darnell Lynch <darnell.lynch@gmail.com>
2014-12-27 14:48:21 -0500

    database connected

--- ideation/settings.py
+++ ideation/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'ideation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ideation',
+        'HOST': 'dlynchtech.com',
+        'PASSWORD' : 'Deenell31',
+        'USER' : 'darnell',
     }
 }
 
