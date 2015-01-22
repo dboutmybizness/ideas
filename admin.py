ef507fc7974562330cf6ab9288780171daee78f2
Darnell Lynch <darnell.lynch@gmail.com>
2015-01-22 16:33:48 -0500

    clean up

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from ideas.models import *
+
+admin.site.register(Idea)
+admin.site.register(Idea_Type)
+admin.site.register(Note)
+admin.site.register(Recently_Viewed)
