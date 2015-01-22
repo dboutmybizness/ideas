5d02ab7837941443c8a125fd47bccaeaf986d656
Darnell Lynch <darnell.lynch@gmail.com>
2015-01-22 16:33:04 -0500

    moving around structure

--- ideas/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from ideas.models import *
-
-admin.site.register(Idea)
-admin.site.register(Idea_Type)
-admin.site.register(Note)
-admin.site.register(Recently_Viewed)
--- ideas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('ideas.views',
-	url(r'^$', 'dash'),
-	url(r'^(?P<id>\d+)/$', 'idea_landing'),
-	url(r'^list/$', 'ideas_main'),
-)
